2ab105e0c7eb7d1515ba56589b4cf64639614bb5
Mridul-2003 <110475111+Mridul-2003@users.noreply.github.com>
2023-04-26 21:54:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import {Route,Routes} from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home/home';
 import About from './components/About';
-import Contact from '/Users/useradmin/Desktop/React-Portfolio/Portfolio/src/components/Contact';
-import Projects from '/Users/useradmin/Desktop/React-Portfolio/Portfolio/src/components/Projects';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
 function App() {
   return (
     <>
